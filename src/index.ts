2f1037d443eba1c07e429f98d73fe64106f8f080
Santiago Martín Agra <santiagomartinagra@gmail.com>
2024-04-03 22:29:27 +0200

    chore: test adding/removing dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
 #!/usr/bin/env tsx
 
-import { intro, outro } from '@clack/prompts';
+import { intro, outro, spinner, note, confirm, cancel, isCancel } from '@clack/prompts';
+import { addDependency, removeDependency, detectPackageManager } from "nypm";
+import color from 'picocolors';
 
-intro(`create-my-app`);
-// Do stuff
+intro(color.underline(`@santima10/lazy`));
+
+const packageManager = await detectPackageManager('.')
+
+note(`package manager detected, using: ${packageManager?.name}`)
+
+const shouldInstall = await confirm({
+  message: 'Do you want install a dependency?',
+});
+
+if (isCancel(shouldInstall)) {
+  const s = spinner();
+  s.start(`Installing via ${packageManager?.name}`);
+  await addDependency('@santima10/jest-chat-reporter', {silent: true})
+  s.stop(`Installed via ${packageManager?.name}`);
+}
+
+const shouldUninstall = await confirm({
+  message: 'Do you want uninstall a dependency?',
+});
+
+if (isCancel(shouldUninstall)) {
+  const s = spinner();
+  s.start(`Uninstalling via ${packageManager?.name}`);
+  await removeDependency('@santima10/jest-chat-reporter', {silent: true})
+  s.stop(`Uninstalled via ${packageManager?.name}`);
+}
+
 outro(`You're all set!`);
